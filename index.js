bf49b1d64a3b16e340eba9f39437762e38577a1f
felix maati <maatifelix001@gmail.com>
2024-07-06 02:06:54 +0300

    short message

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Write your solution in this file!
+var customerName = 'bob';
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+const leastFavoriteCustomer = 'some initial value';
+
+function changeLeastFavoriteCustomer() {
+    try {
+        leastFavoriteCustomer = 'new value';
+    } catch (error) {
+        console.log("Error: Cannot reassign a constant variable");
+    }
+}
